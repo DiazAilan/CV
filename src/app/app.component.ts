c4f06d28c4c08a6034b1c9bead12c1d4c1f69707
Gonzalo Díaz Ailán <gonzalo_devlain@hotmail.com>
2016-12-30 09:48:54 -0300

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Que buena app posta!';
+  skills = [{
+  	name: 'HTML5',
+  	skill: 100
+  },{
+  	name: 'CSS3',
+  	skill: 100
+  },{
+  	name: 'Javascript',
+  	skill: 90
+  },{
+  	name: 'jQuery',
+  	skill: 75
+  },{
+  	name: 'SASS',
+  	skill: 80
+  },{
+  	name: 'Bootstrap 3',
+  	skill: 90
+  },{
+  	name: 'AngularJS',
+  	skill: 90
+  },{
+  	name: 'Angular 2',
+  	skill: 65
+  },{
+  	name: 'Ionic 1.x',
+  	skill: 90
+  },{
+  	name: 'Ionic 2',
+  	skill: 80
+  },{
+  	name: 'Photoshop',
+  	skill: 85
+  },{
+  	name: 'GIMP',
+  	skill: 60
+  },{
+  	name: 'iBeacon',
+  	skill: 100
+  },{
+  	name: 'Eddystone',
+  	skill: 70
+  },{
+  	name: 'UI/UX',
+  	skill: 90
+  },{
+  	name: 'SEO',
+  	skill: 100
+  },{
+  	name: 'Responsive Web Design',
+  	skill: 90
+  },{
+  	name: 'Google Analytics',
+  	skill: 80
+  }]
 }
